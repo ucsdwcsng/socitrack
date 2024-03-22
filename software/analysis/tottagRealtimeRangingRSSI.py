ea5d47ef1e0fa2f0c412af7b011eb140301396e7
ReSummit <dmvo@ucsd.edu>
2024-03-21 20:24:06 -0700

    Fix logging errors for RSSI

--- software/analysis/tottagRealtimeRangingRSSI.py
+++ software/analysis/tottagRealtimeRangingRSSI.py
@@ -17,6 +17,9 @@ EUI_LENGTH = 1
 RANGE_DATA_LENGTH = EUI_LENGTH + 4
 TOTTAG_DATA_UUID = 'd68c3153-a23f-ee90-0c45-5231395e5d2e'
 
+# No more than 30 RSSIs per device
+NUM_RSSI_ENTRIES = 1
+
 
 # STATE VARIABLES -----------------------------------------------------------------------------------------------------
 
@@ -25,12 +28,13 @@ data_characteristics = []
 ranging_files = []
 tottags = []
 
-cur_tags = []
+cur_tags = {}
 
 # HELPER FUNCTIONS AND CALLBACKS --------------------------------------------------------------------------------------
 
-def data_received_callback(data_file, sender_characteristic, data):
+def data_received_callback(data_file, addr, sender_characteristic, data):
   identifier = data[0]
+  # Get last 2 bytes of address
   if identifier == 14:
     num_ranges, from_eui = struct.unpack('<BB', data[1:3])
     timestamp = struct.unpack('<I', data[(3+num_ranges*5):(7+num_ranges*5)])[0]
@@ -40,18 +44,16 @@ def data_received_callback(data_file, sender_characteristic, data):
       print('\tDevice {} with millimeter range {}'.format(hex(to_eui)[2:], range_mm))
       # Get OS timestamp in seconds
       osTime = int(datetime.now().timestamp())
-
-      cur_tags.append({'to': to_eui, 'from': from_eui, 'range': range_mm, 'timestamp': timestamp, 'ostime': osTime, 'num_ranges': num_ranges})
+      cur_tags[addr]=({'to': to_eui, 'from': from_eui, 'range': range_mm, 'timestamp': timestamp, 'ostime': osTime, 'num_ranges': num_ranges})
   elif identifier == 15:
     if len(cur_tags) == 0 or len(data[1:]) < 120:
       return
     # Get RSSI data entries starting from 2nd byte as list of floats
-    rssi_dat = len(data[1:])
-    rssi_fmt = '<' + 'f'*cur_tags[0]['num_ranges']*30
+    rssi_fmt = '<' + 'f'*cur_tags[addr]['num_ranges']*30
     rssi_struct = struct.unpack(rssi_fmt, data[1:121])
     # data_file.write('{}\t{}\t{}\t{}\t{}'.format(osTime, timestamp, hex(from_eui)[2:], hex(to_eui)[2:], range_mm))
-    data_file.write('{}\t{}\t{}\t{}\t{}'.format(cur_tags[0]['ostime'], cur_tags[0]['timestamp'], hex(cur_tags[0]['from'])[2:], hex(cur_tags[0]['to'])[2:], cur_tags[0]['range']))
-    for i in range(30):
+    data_file.write('{}\t{}\t{}\t{}\t{}'.format(cur_tags[addr]['ostime'], cur_tags[addr]['timestamp'], hex(cur_tags[addr]['from'])[2:], hex(cur_tags[addr]['to'])[2:], cur_tags[addr]['range']))
+    for i in range(NUM_RSSI_ENTRIES):
       # Write to file, truncate to 4 decimal places
       data_file.write('\t{}'.format(round(rssi_struct[i], 4)))
     data_file.write('\n')
@@ -86,14 +88,14 @@ async def log_ranges():
               try:
                 file = open(filename_base + client.address.replace(':', '') + '.data', 'w')
                 file.write('UNIX\tTimestamp\tFrom\tTo\tDistance (mm)\n')
-                for i in range(30):
+                for i in range(NUM_RSSI_ENTRIES):
                   file.write('\tRSSI_{}'.format(i))
                 file.write('\n')
               except Exception as e:
                 print(e)
                 print('ERROR: Unable to create a ranging data log file')
                 sys.exit('Unable to create a ranging data log file: Cannot continue!')
-              await client.start_notify(characteristic, functools.partial(data_received_callback, file))
+              await client.start_notify(characteristic, functools.partial(data_received_callback, file, client.address))
               data_characteristics.append(characteristic)
               ranging_files.append(file)
               tottags.append(client)
